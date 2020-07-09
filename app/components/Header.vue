91305025dcbdf5949ee3c93c13a5f4a4f6af2d02
Takami Shiraishi <misris427@gmail.com>
2020-07-09 13:02:41 -0700

    add scss

---
+++ app/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div class="menu">
+      <div class="menu-content">
+        menu-content
+        <HeaderItem />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({})
+</script>
+
+<style lang="scss">
+</style>
---
+++ app/components/HeaderItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header-item">
+    itemitemitem
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({})
+</script>
+
+<style>
+  .header-item {
+    color: red;
+  }
+</style>
